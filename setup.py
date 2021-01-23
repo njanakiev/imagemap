77b30bae9bbc7cf09bd471f1042a87e546f21c61
Nikolai Janakiev <nikolai.janakiev@gmail.com>
2021-01-24 00:01:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name='imagemap',
     version='0.0.0',
-    url='https://github.com/njanakiev/imagemap.git',
-    author='Author Name',
+    url='https://gitlab.com/njanakiev/imagemap.git',
+    author='Nikolai Janakiev',
     author_email='nikolai.janakiev@gmail.com',
     description='imagemap',
     platforms='any',
-    packages=['imagemap'],    
+    packages=['imagemap'],
     install_requires=[],
 )
